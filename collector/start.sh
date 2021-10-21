c2f5da1c26931e16fae8d2098d36a68f95beeb33
santanude <santanu.de@xoriant.com>
2021-10-21 17:42:34 +0530

    Add support for measurement event related to network slicing
    This task will add the JSON schema changes for that measurement event, and the corresponding support for it in the VES collector to validate the event based on the schema. In addition, it will also add support for persisting data into data sinks such as InfluxdB, Kafka, Elasticsearch.
    
    Issue-Id: SMO-21
    Signed-off-by: santanude <santanu.de@xoriant.com>
    Change-Id: I5a866e1b3735c51bb0d0e34c15053b37493d714b
    Signed-off-by: santanude <santanu.de@xoriant.com>

--- collector/start.sh
+++ collector/start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# Original work Copyright 2017-2018 AT&T Intellectual Property, Inc
-# Modified work Copyright 2021 Xoriant Corporation
+# Copyright 2017-2018 AT&T Intellectual Property, Inc
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,32 +13,54 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# What this is: Startup script for the VES Collector running under docker.
+#. What this is: Startup script for the OPNFV VES Collector running under docker.
 # the variables used below are now passed in as environmental variables
 # from the docker run command.
 cd /opt/ves
 touch monitor.log
 
+config_file="evel-test-collector/config/collector.conf"
+schema_file=`grep -w schema_file $config_file | head -1 | cut -d "=" -f 2 | xargs`
+
+echo  "schema_file = " $schema_file  > monitor.log
+if [ "$schema_file" != "" ]; then
+        if ! [ -e $schema_file ]; then
+                echo "Schema file does not exists!" >> monitor.log
+		exit
+        fi
+else
+        echo "Schema file path is missing in config file!" >> monitor.log
+	exit
+fi
+
 sed -i -- \
   "s~log_file = /var/log/att/collector.log~log_file = /opt/ves/collector.log~" \
-  evel-test-collector/config/collector.conf
+  $config_file
 sed -i -- "s/vel_domain = 127.0.0.1/vel_domain = $ves_host/g" \
-  evel-test-collector/config/collector.conf
+  $config_file
 sed -i -- "s/vel_port = 30000/vel_port = $ves_port/g" \
-  evel-test-collector/config/collector.conf
+  $config_file
 sed -i -- "s/vel_username =/vel_username = $ves_user/g" \
-  evel-test-collector/config/collector.conf
+  $config_file
 sed -i -- "s/vel_password =/vel_password = $ves_pass/g" \
-  evel-test-collector/config/collector.conf
+  $config_file
 sed -i -- "s~vel_path = vendor_event_listener/~vel_path = $ves_path~g" \
-  evel-test-collector/config/collector.conf
+  $config_file
 sed -i -- "s~vel_topic_name = example_vnf~vel_topic_name = $ves_topic~g" \
-  evel-test-collector/config/collector.conf
+  $config_file
 sed -i -- "/vel_topic_name = /a influxdb = $ves_influxdb_host:$ves_influxdb_port" \
-  evel-test-collector/config/collector.conf
+  $config_file
+sed -i -- "s/elasticsearch_domain =/elasticsearch_domain = $elasticsearch_domain/g" \
+  $config_file
+sed -i -- "s/data_storage =/data_storage = $data_storage/g" \
+  $config_file
+sed -i -- "s/kafka_second_port =/kafka_second_port = $kafka_host_2:$kafka_port_2/g" \
+  $config_file
+sed -i -- "s/kafka_topic =/kafka_topic = $kafka_topic/g" \
+  $config_file
 
-echo; echo "evel-test-collector/config/collector.conf"
-cat evel-test-collector/config/collector.conf
+echo; echo $config_file
+cat $config_file
 
 echo; echo "wait for InfluxDB API at $ves_influxdb_host:$ves_influxdb_port"
 while ! curl http://$ves_influxdb_host:$ves_influxdb_port/ping ; do
