3f17d2a1affd90c319aeb3e1f4608e6b2228fc09
santanude <santanu.de@xoriant.com>
2021-11-02 17:55:00 +0530

    Add connector between Kafka bus and InfluxdB to sync VES events
    VES events will be written to Kafka bus by default. However, if an InfluxdB exists in SMO, then the event needs to be synced to InfluxdB also.
    
    Issue-Id: SMO-22
    Signed-off-by: santanude <santanu.de@xoriant.com>
    Change-Id: I152b0ad5a7b6676eef702e3c3811c2f381b0f4f8

---
+++ kafka-connector-influxdb/start.sh
@@ -0,0 +1,49 @@
+# Copyright 2021 Xoriant Corporation
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+cd /opt/ves
+touch monitor.log
+
+config_file="influxdb-connector/config/influxdb_connector.conf"
+
+sed -i -- "s/influxdb =/influxdb = $ves_influxdb_host:$ves_influxdb_port/g" \
+  $config_file
+sed -i -- "s/kafka_server =/kafka_server = $kafka_host_2:$kafka_port_2/g" \
+  $config_file
+
+echo; echo $config_file
+cat $config_file
+
+echo; echo "wait for InfluxDB API at $ves_influxdb_host:$ves_influxdb_port"
+while ! curl http://$ves_influxdb_host:$ves_influxdb_port/ping ; do
+  echo "InfluxDB API is not yet responding... waiting 10 seconds"
+  sleep 10
+done
+
+echo; echo "setup veseventsdb in InfluxDB"
+# TODO: check if pre-existing and skip
+curl -X POST http://$ves_influxdb_host:$ves_influxdb_port/query \
+  --data-urlencode "q=CREATE DATABASE veseventsdb"
+
+if [ "$ves_loglevel" != "" ]; then
+  python3 /opt/ves/influxdb-connector/code/influxdb_connector.py \
+    --config /opt/ves/influxdb-connector/config/influxdb_connector.conf \
+    --influxdb $ves_influxdb_host:$ves_influxdb_port \
+    --section default > /opt/ves/monitor.log 2>&1
+else
+  python3 /opt/ves/influxdb-connector/code/influxdb_connector.py  \
+    --config /opt/ves/influxdb-connector/config/influxdb_connector.conf \
+    --influxdb $ves_influxdb_host:$ves_influxdb_port \
+    --section default
+fi
